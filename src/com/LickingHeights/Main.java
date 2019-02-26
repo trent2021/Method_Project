4e91f24e91e125b6583fa8fbdc5bdb8e68d748d8
Trent <trent.polenchar.2021@student.lhschools.org>
2019-02-26 13:40:17 -0500

    Method one (Hello World)

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,7 +3,14 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
+        hello();
 
-        
+
+
+
+    }
+
+    public static void hello(){
+        System.out.println("Hello World");
     }
 }
