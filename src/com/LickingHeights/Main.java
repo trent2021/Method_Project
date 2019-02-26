92ed447344332017077aed98539ef384fe4701d4
Trent <trent.polenchar.2021@student.lhschools.org>
2019-02-26 14:02:42 -0500

    Method two (Hello name)

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,16 +1,44 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        helloWorld();
+
         hello();
+
+
+
+
+
+
 
 
 
 
     }
 
-    public static void hello(){
+    public static void helloWorld() {
         System.out.println("Hello World");
+    }
+
+
+    public static void hello(){
+        String name;
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+        name = keyboard.nextLine();
+        System.out.println("Hello " + name);
+        return;
+
+
+
+
+
     }
 }
